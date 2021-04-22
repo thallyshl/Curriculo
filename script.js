dfb949015ff194e1272a62b7656372afa5bd2482
Thallys Henrique <79019903+thallyshl@users.noreply.github.com>
2021-04-21 23:59:10 -0300

    Commit

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 //var name = window.prompt("Olá, qual o seu nome?")
 //    alert ("Espero que goste do meu currículo, " + name + "!") // Não esquecer de concatenar "+"
 
+let show = true;
+const menuSection = document.querySelector(".menu-section")
+const mainMobile = document.querySelector(".main-top_mobile")
+const mainItems = document.querySelector ("main-items")
+
 function menuMobile() {
-    
+    document.body.style.overflow = show ? "hidden" : "initial"
+    menuSection.classList.toggle("on", show);
+    show = !show;
+    console.log(show)
+}
+function mobileTop () {
+    document.body.style.overflow = show ? "hidden" : "initial"
+    menuSection.classList.toggle("on", show);
+    show = !show;
 }
