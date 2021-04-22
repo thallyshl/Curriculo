37a15e7b3332031b7b0c7e8fc0f58fe84ca88bb9
Thallys Henrique <79019903+thallyshl@users.noreply.github.com>
2021-04-21 22:04:23 -0300

    commit

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 //var name = window.prompt("Olá, qual o seu nome?")
 //    alert ("Espero que goste do meu currículo, " + name + "!") // Não esquecer de concatenar "+"
+
+function menuMobile() {
+    
+}
