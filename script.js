5f0652af96dbd37ca89f64a3d7beec25a4a0765b
Thallys Henrique <79019903+thallyshl@users.noreply.github.com>
2021-04-22 00:39:37 -0300

    commit

--- script.js
+++ script.js
@@ -3,13 +3,16 @@
 
 let show = true;
 const menuSection = document.querySelector(".menu-section")
+const paragraphMenu = document.querySelector(".teste")
 //const mainMobile = document.querySelector(".main-top_mobile")
 //const mainItems = document.querySelector (".main-items")
 
 function menuMobile() {
     document.body.style.overflow = show ? "hidden" : "initial"
+    paragraphMenu.innerHTML = show ? "&#x2716;" : "&equiv;"
     menuSection.classList.toggle("on", show);
     show = !show;
+    
     console.log(show)
 }
 function mobileTop () {
